12461cfde8b553d5c4768441d27513860525670f
“Alyssa <agoody44@gmail.com>
2020-10-27 22:18:41 -0400

    randomized answer order

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
+const baseURL = `https://opentdb.com/api.php?amount=15&category=20&type=multiple`;
+// const scoreEl = document.querySelector('.scoreboard. score-num');
+// const answeredEl = document.querySelector('.scoreBoard .answer-num');
+
+
+
+const startButton = document.getElementById('startBtn');
+const questionContainerElement = document.getElementById('question-container');
+
+startButton.addEventListener('click', startGame);
+
+function startGame () {
+    console.log('Started');
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+}
+
+
+
 window.onload = sendApiRequest
 
 async function sendApiRequest() {
-    let response = await fetch(`https://opentdb.com/api.php?amount=15&category=20&type=multiple`);
+    let response = await fetch(baseURL);
     console.log(response)
     let data = await response.json()
     console.log(data);
@@ -13,24 +32,48 @@ function useApiData (data) {
     document.querySelector('#category').innerHTML = `Category: ${data.results[0].category}`
     document.querySelector('#difficulty').innerHTML = `Difficulty: ${data.results[0].difficulty}`
     document.querySelector('#questions').innerHTML = `Question: ${data.results[0].question}`
+    
+    
+    const answers = [data.results[0].correct_answer, data.results[0].incorrect_answers[0], data.results[0].incorrect_answers[1], data.results[0].incorrect_answers[2]]
+    const answerElements = [
+        document.getElementById("answer1"),
+        document.getElementById("answer2"),
+        document.getElementById("answer3"),
+        document.getElementById("answer4")
+      ];
+      
+      // Shuffles the answers
+      randomize(answers);
+      
+      // Loops 
+      for(let i = 0; i < answers.length; i++){
+        if(answerElements[i] && answerElements[i].textContent){
+          answerElements[i].textContent = answers[i];
+        }
+      }
+      
+      // Implements shuffle
+      function randomize(array){
+        for(let i = array.length - 1; i > 0; i--){
+          const index = Math.floor(Math.random() * i);
+          const tmp = array[index];
+          array[index] = array[i];
+          array[i] = tmp;
+        }
+      }
+    }
+// let correctButton = document.querySelector('answer1')
 
-    document.querySelector('#answer1').innerHTML = data.results[0].correct_answer
-    document.querySelector('#answer2').innerHTML = data.results[0].incorrect_answers[0]
-    document.querySelector('#answer3').innerHTML = data.results[1].incorrect_answers[0]
-    document.querySelector('#answer4').innerHTML = data.results[2].incorrect_answers[0]
-}
 
 
-let correctButton = document.querySelector('#answer1')
-let incorrectButton = document.querySelector('#answer2')
+// correctButton.addEventListener('click', ()=>{
+//     alert('correct!');
+//     sendApiRequest ()
+// })
 
-
-correctButton.addEventListener('click', ()=>{
-    alert('correct!');
-    sendApiRequest ()
-})
-
-incorrectButton.addEventListener('click', ()=>{
-    alert('wrong!');
-    sendApiRequest ()
-})
+// incorrectButton.addEventListener('click', ()=>{
+//     alert('wrong!');
+//     sendApiRequest ()
+// });
+// }
+    
